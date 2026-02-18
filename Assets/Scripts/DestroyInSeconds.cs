7f2bcc97db32daf3819968d75d3274280d7b9fb3
xXcallmemagicXx <eduardo.franco@uteca.edum.mx>
2026-02-17 20:43:46 -0600

    Pockemon_Battle

---
+++ Assets/Scripts/DestroyInSeconds.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DestroyInSeconds : MonoBehaviour
+{
+   [SerializeField]
+
+   private float secondsToDestroy = 1f;
+
+   private void OnEnable()
+   {
+    Invoke(nameof(DestroySelf), secondsToDestroy);
+   }
+
+   private void DestroySelf()
+   {
+    gameObject.SetActive(false);
+   }
+}
